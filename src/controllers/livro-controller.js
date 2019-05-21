8cfe5da3e67599acb05ed38a2c371ee006793f29
Patrick Bathu <patrick.bathu@compasso.com.br>
2019-05-21 14:36:30 -0300

    Operações finalizadas
    - Salvar
    - Listar
    - Deletar
    - Atualizar
    
    Usando o banco mongo local

--- src/controllers/livro-controller.js
+++ src/controllers/livro-controller.js
@@ -32,3 +32,29 @@ exports.get = async(req, res, next) => {
         });
     }
 }
+
+exports.put = async(req, res, next) => {
+    try {
+        await repository.update(req.params.id, req.body);
+        res.status(200).send({
+            message: 'Produto atualizado com sucesso!'
+        });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }
+};
+
+exports.delete = async(req, res, next) => {
+    try {
+        await repository.delete(req.body._id)
+        res.status(200).send({
+            message: 'Produto removido com sucesso!'
+        });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }
+};
--- src/routes/livro-route.js
+++ src/routes/livro-route.js
@@ -6,5 +6,7 @@ const controller = require('../controllers/livro-controller');
 
 router.post('/', controller.post);
 router.get('/', controller.get);
+router.put('/:id', controller.put);
+router.delete('/', controller.delete);
 
 module.exports = router;

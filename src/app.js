da8df84e412693fc3a0d1e73df9e4fc371c12d5b
patrickbathu <patrickbathu@outlook.com>
2019-06-10 22:10:12 -0300

    Update

--- src/app.js
+++ src/app.js
@@ -28,6 +28,13 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 //ROTAS
 app.use('/', indexRoute);
 app.use('/v1/livro', livroRoute);
